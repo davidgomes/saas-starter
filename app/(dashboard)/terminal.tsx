27d68b5a714cbad3dbdbd86743d25d0318df155e
David Gomes <dgomes@anysphere.co>
2025-09-30 17:28:25 -0700

    Change app color to green

--- app/(dashboard)/terminal.tsx
+++ app/(dashboard)/terminal.tsx
@@ -37,7 +37,7 @@ export function Terminal() {
         <div className="flex justify-between items-center mb-4">
           <div className="flex space-x-2">
             <div className="w-3 h-3 rounded-full bg-red-500"></div>
-            <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
+            <div className="w-3 h-3 rounded-full bg-green-400"></div>
             <div className="w-3 h-3 rounded-full bg-green-500"></div>
           </div>
           <button
